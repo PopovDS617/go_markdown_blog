41b570175597c6935fc95c88beef4a4bab83fbe6
Popov Dmitry <popovds.bm@gmail.com>
2024-04-04 17:32:59 +0300

    feat: add logging middleware

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"gomarkdownblog/internal/logger"
+	"gomarkdownblog/internal/middleware"
+	"os"
+
 	"log"
 	"net/http"
 )
 
 func main() {
+	httpPort := os.Getenv("HTTP_PORT")
+
 	logger := logger.Init()
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /", middleware.LoggingMiddleware(func(w http.ResponseWriter, r *http.Request) {
 
-		logger.Info(r.URL.Path)
+	}, logger))
 
-	})
-
-	if err := http.ListenAndServe(":8000", mux); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", httpPort), mux); err != nil {
 		log.Fatal(err)
 	}
 }
