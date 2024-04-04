6a1e3c30382be3058567cf9aa8d8a585bd77a962
Popov Dmitry <popovds.bm@gmail.com>
2024-04-04 16:58:03 +0300

    feat: add logger

--- cmd/server.go
+++ cmd/server.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"gomarkdownblog/internal/logger"
 	"log"
 	"net/http"
 )
 
 func main() {
+	logger := logger.Init()
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+
+		logger.Info(r.URL.Path)
+
+	})
 
 	if err := http.ListenAndServe(":8000", mux); err != nil {
 		log.Fatal(err)
---
+++ internal/logger/logger.go
@@ -0,0 +1,60 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// exports as logger
+var globalLogger *zap.Logger
+
+func Init() *zap.Logger {
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.TimeKey = "timestamp"
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	config := zap.Config{
+		Level:             zap.NewAtomicLevelAt(zap.InfoLevel),
+		Development:       false,
+		DisableCaller:     false,
+		DisableStacktrace: false,
+		Sampling:          nil,
+		Encoding:          "json",
+		EncoderConfig:     encoderCfg,
+		OutputPaths: []string{
+			"stderr",
+		},
+		ErrorOutputPaths: []string{
+			"stderr",
+		},
+		// InitialFields: map[string]interface{}{
+		// 	"pid": os.Getpid(),
+		// },
+	}
+
+	return zap.Must(config.Build())
+}
+
+func Debug(msg string, fields ...zap.Field) {
+	globalLogger.Debug(msg, fields...)
+}
+
+func Info(msg string, fields ...zap.Field) {
+	globalLogger.Info(msg, fields...)
+}
+
+func Warn(msg string, fields ...zap.Field) {
+	globalLogger.Warn(msg, fields...)
+}
+
+func Error(msg string, fields ...zap.Field) {
+	globalLogger.Error(msg, fields...)
+}
+
+func Fatal(msg string, fields ...zap.Field) {
+	globalLogger.Fatal(msg, fields...)
+}
+
+func WithOptions(opts ...zap.Option) *zap.Logger {
+	return globalLogger.WithOptions(opts...)
+}
